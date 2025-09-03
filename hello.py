cdf8dbb345fe6d59a79e73b7a9e074ca1ab0b8c8
rfeuchtw2 <rfeuchtw2@student.touro.edu>
2025-09-02 22:28:31 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,17 @@
+Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 09:44:33) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('hello world')
+hello world
+>>> def hello ():
+	print('hello all')
+
+	
+>>> hello()
+hello all
+>>> def hello2 (name):
+	print ('hello', name, '!')
+
+	
+>>> hello2 ('MCON 141!!!')
+hello MCON 141!!! !
+>>> 
